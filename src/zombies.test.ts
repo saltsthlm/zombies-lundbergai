c9394b71c01634e4068b4a019638e27c3e6c76eb
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-16 17:18:19 +0200

    add test comments and remove dead code

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -3,18 +3,21 @@ import { test } from "node:test";
 import { IRoom } from "./interfaces";
 import { addZombie, createRoom, isRoomFull } from "./zombies";
 
+// test 1
 test("room is full", () => {
   const room: IRoom = createRoom(0);
 
   ok(isRoomFull(room));
 });
 
+// test 2
 test("empty room that fits one zombie is not full", () => {
   const room: IRoom = createRoom(1);
 
   ok(!isRoomFull(room));
 });
 
+// test 3
 test("room with no capacity cannot fit any zombies", () => {
   const room: IRoom = createRoom(0);
 
@@ -23,22 +26,25 @@ test("room with no capacity cannot fit any zombies", () => {
   ok(!added);
 });
 
+// test 4
 test("one-roomer becomes full when a zombie is added", () => {
   const room: IRoom = createRoom(1);
 
-  const added: boolean = addZombie(room, "Viktor");
+  addZombie(room, "Viktor");
 
   ok(isRoomFull(room));
 });
 
+// test 5
 test("two-roomer is not full when a zombie is added", () => {
   const room: IRoom = createRoom(2);
 
-  const added: boolean = addZombie(room, "Viktor");
+  addZombie(room, "Viktor");
 
   ok(!isRoomFull(room))
 });
 
+// test 6
 test("second zombie consumes first zombie when added to a one-roomer", () => {
   const room: IRoom = createRoom(1);
 
@@ -50,6 +56,7 @@ test("second zombie consumes first zombie when added to a one-roomer", () => {
   ok(room.zombies[0].name == secondZombieName);
 });
 
+// test 7
 test("negative capacity behaves as empty room", () => {
   const room: IRoom = createRoom(-1);
 
