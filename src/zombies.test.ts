ec340c0dfa1170df43fa2b4d93646358ae11bf52
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-16 15:08:04 +0200

    fix failing test: implement shifting when reached full capacity

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -24,11 +24,20 @@ const isRoomFull = (room: IRoom) => {
 const addZombie = (room: IRoom, name: string): boolean => {
   const zombie: IZombie = { name: name };
 
+  // no capacity at all means nothing can be added
+  if (room.capacity <= 0) {
+    return false;
+  }
+
+  // capacity not reached
   if (room.zombies.length < room.capacity) {
     room.zombies.push(zombie);
     return true;
   } else {
-    return false;
+    // full room, remove first element and move down all elements one position
+    room.zombies.shift();
+    room.zombies.push(zombie);
+    return true;
   }
 }
 
