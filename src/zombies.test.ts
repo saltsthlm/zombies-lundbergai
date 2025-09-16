fcc31cc42241573de5582c3815aa6c4d42cd890c
Viktor Lundberg <vikkolundberg@gmail.com>
2025-09-16 12:14:33 +0200

    create IRoom interface in "room is full" test

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -1,30 +1,34 @@
 import { ok } from "node:assert/strict";
 import { test } from "node:test";
 
-const createRoom = (capacity: number) => {
-  const _capacity = capacity;
+interface IRoom {
+  capacity: number;
+};
 
+const createRoom = (capacity: number): IRoom => {
   return {
-    isFull: () => true,
-  };
+    capacity: capacity
+  }
+};
+
+const isRoomFull = (room: IRoom) => {
+  return true;
 };
 
 test("room is full", () => {
-  const room = createRoom(0);
+  const room: IRoom = createRoom(0);
 
-  const isRoomFull = room.isFull();
-
-  ok(isRoomFull);
+  ok(isRoomFull(room));
 });
 
-test.skip("empty room that fits one zombie is not full", () => {});
+test.skip("empty room that fits one zombie is not full", () => { });
 
-test.skip("room with no capacity cannot fit any zombies", () => {});
+test.skip("room with no capacity cannot fit any zombies", () => { });
 
-test.skip("one-roomer becomes full when a zombie is added", () => {});
+test.skip("one-roomer becomes full when a zombie is added", () => { });
 
-test.skip("two-roomer is not full when a zombie is added", () => {});
+test.skip("two-roomer is not full when a zombie is added", () => { });
 
-test.skip("second zombie consumes first zombie when added to a one-roomer", () => {});
+test.skip("second zombie consumes first zombie when added to a one-roomer", () => { });
 
 // You are free to add more tests that you think are relevant!
